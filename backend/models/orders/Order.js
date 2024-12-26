b91ecf3995c23118d9d5f10193b05f1a8f424b35
Salma Ahmed <108466703+Salmaahmed8@users.noreply.github.com>
2024-12-26 23:09:40 +0200

    feat: create OrderSchema

---
+++ backend/models/orders/Order.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Uder',
+        required: true
+    },
+    items: [{
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product'
+        },
+        quantity: Number,
+        price: Number
+    }],
+    total: {
+        type: Number,
+        required: true
+    },
+    shippingAddress: {
+        type: String,
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'processing', 'shipped', 'delivered'],
+        default: 'pending'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const Order = mongoose.model('Order', orderSchema);
+export default Order;
