d972a0af539e6cd9f5f41b5e7836f49a043b32fe
Salma Ahmed <108466703+Salmaahmed8@users.noreply.github.com>
2024-12-28 00:01:00 +0200

    feat: add features for dealing with product

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import connect from "./backend/db/connect.js";
 import cookieParser from "cookie-parser";
 import fs from "node:fs";
+import path from 'path';
+import compression from 'compression';
+
+
+import connect from "./backend/db/connect.js";
 import errorHandler from "./backend/helpers/errorhandler.js"
+import productRoutes from './backend/routes/productRoutes.js';
+import userRoutes from './backend/routes/userRoutes.js';
+import orderRoutes from './backend/routes/orderRoutes.js';
+import uploadRoutes from './backend/routes/uploadRoutes.js';
+import paymentRoutes from './backend/routes/paymentRoutes.js';
 
 dotenv.config();
 
@@ -25,9 +34,11 @@ app.use(cookieParser());
 //error handler middleware
 app.use(errorHandler)
 
+const __dirname = path.resolve(); // Set {__dirname} to current working directory
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 //routes
 const routeFiles = fs.readdirSync(`./backend/routes`);
-
 routeFiles.forEach((file) => {
     // use dynamic import
     import(`./backend/routes/${file}`)
@@ -38,6 +49,13 @@ routeFiles.forEach((file) => {
         console.log("Failed to load route file", err);
       });
   });
+
+app.use('/api/v1/products', productRoutes);
+app.use('/api/v1/users', userRoutes);
+app.use('/api/v1/orders', orderRoutes);
+app.use('/api/v1/upload', uploadRoutes);
+app.use('/api/v1/payment', paymentRoutes);
+
 
 const server = async () => {
     try {
